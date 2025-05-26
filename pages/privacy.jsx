ada4f8253018505dc8d1b82b80c2cfd6330b52d8
Shex Fuad <136645067+shexfuad404@users.noreply.github.com>
2025-05-26 14:31:22 +0300

    privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,18 +2,18 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for MusicMaker</h1>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Sloth Music</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the MusicMaker bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ To use the Sloth Music bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(ADMINISTRATION)</a></p>
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot when using certain commands from the bot commands.</p>
+        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/800442243697213442/vote">Sloth Music</a> bot when using certain commands from the bot commands.</p>
         <div className="py-3"></div>
-        <p>⊳ Every user can use the MusicMaker bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ Every user can use the Sloth Music bot however they want, as long as they do not intend to harm.</p>
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using MusicMaker.</p>   
+        <p>⊳ You cannot use certain commands at the same time while using Sloth Music.</p>   
         <div className="py-3"></div>
-        <p>⊳ MusicMaker bot does not save any data based on users.</p>
+        <p>⊳ Sloth Music bot does not save any data based on users.</p>
         <div className="py-3"></div>
         <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
     
